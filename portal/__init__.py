30e30cd418ee9b103e04916e19deb9902d3aafd8
elijah-scott <elijah@globus.org>
2023-02-28 18:38:34 -0500

    fix redirect to port 5000
    When using a domain name behind a reverse proxy, it would reirect to
    port 5000

--- portal/__init__.py
+++ portal/__init__.py
@@ -8,7 +8,7 @@ __author__ = 'Globus Team <info@globus.org>'
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1
 )
 
 app.config.from_pyfile('portal.conf')
